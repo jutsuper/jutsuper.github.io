f947b2abdc71bad4076d2079db973e8dfe222277
kerdl <clayshowtv@gmail.com>
2024-12-05 00:45:23 +0300

    autoplay instructions for each browser, #faq-autoplay scroll into view

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const LOGGING_ENABLED = true;
 const TRANSLATIONS_PATH = "translations";
+//const OVERRIDE_LANGUAGE = "ru-RU";
+const OVERRIDE_LANGUAGE = null;
 const IGNORED_TRANSLATIONS = [
   "en-US"
 ]
@@ -33,10 +35,12 @@ class JutSuper {
   /**
    * @param {Document} doc 
    * @param {Navigator} nav
+   * @param {Location} loc
    */
-  constructor(doc, nav) {
+  constructor(doc, nav, loc) {
     this.doc = doc;
-    this.nav = nav
+    this.nav = nav;
+    this.loc = loc;
 
     this.body = this.doc.getElementsByTagName("body")[0];
     this.loadingOverlay = this.doc.querySelector(".jsp-loading-overlay");
@@ -46,6 +50,12 @@ class JutSuper {
       this.doc.querySelector("#jsp-faq-bar-switch-3"),
       this.doc.querySelector("#jsp-faq-bar-switch-4")
     ];
+    this.faqAutoplayBrowsersInputs = [
+      this.doc.querySelector("#jsp-faq-bar-switch-2-1"),
+      this.doc.querySelector("#jsp-faq-bar-switch-2-2"),
+      this.doc.querySelector("#jsp-faq-bar-switch-2-3"),
+      this.doc.querySelector("#jsp-faq-bar-switch-2-4")
+    ];
 
     this.init();
   }
@@ -53,9 +63,23 @@ class JutSuper {
   async init() {
     await this.registerFaqUnfoldListeners();
 
-    const translations = await this.fetchPrefferedTranslations();
-    if (translations) {
-      await this.translatePage(translations);
+    const textTranslations = await this.fetchPrefferedTextTranslations();
+    if (textTranslations) {
+      await this.translatePageText(textTranslations);
+    }
+
+    const imageTranslations = await this.fetchPrefferedImageTranslations();
+    if (imageTranslations) {
+      await this.translatePageImages(imageTranslations);
+    }
+
+    if (this.loc.hash === "#faq-autoplay") {
+      const autoplayFaqInput = this.faqInputs[1];
+      autoplayFaqInput.checked = true;
+      const autoplayFaqTitle = this.doc.getElementsByClassName(
+        "jsp-text-no-autoplayback-q"
+      )[0]
+      autoplayFaqTitle.scrollIntoView({ behavior: "smooth", block: "start" });
     }
 
     await this.hideLoadingOverlay();
@@ -74,13 +98,33 @@ class JutSuper {
         }
       });
     }
+
+    for (const faqInput of this.faqAutoplayBrowsersInputs) {
+      faqInput.addEventListener("change", event => {
+        const target = /** @type {HTMLInputElement} */ (event.target);
+        for (const faqInput of this.faqAutoplayBrowsersInputs) {
+          if (faqInput.id === target.id) {
+            continue;
+          }
+
+          faqInput.checked = false;
+        }
+      });
+    }
   }
 
   /**
    * @returns {Promise<Record<string, string> | undefined>}
    */
-  async fetchPrefferedTranslations() {
-    const lang = "ru-RU";//this.getPrefferedLanguage();
+  async fetchPrefferedTextTranslations() {
+    let lang;
+    if (OVERRIDE_LANGUAGE) {
+      lang = OVERRIDE_LANGUAGE;
+    }
+    else {
+      lang = this.getPrefferedLanguage();
+    }
+    
     if (!lang || IGNORED_TRANSLATIONS.includes(lang)) {
       return;
     }
@@ -97,11 +141,49 @@ class JutSuper {
   /**
    * @param {Record<string, string>} translations
    */
-  async translatePage(translations) {
+  async translatePageText(translations) {
     for (const [cls, text] of Object.entries(translations)) {
       [...this.doc.querySelectorAll(`.${cls}`)].forEach(
         (value, index, array) => {
           value.innerHTML = text;
+        }
+      );
+    }
+  }
+
+  /**
+   * @returns {Promise<Record<string, string> | undefined>}
+   */
+  async fetchPrefferedImageTranslations() {
+    let lang;
+    if (OVERRIDE_LANGUAGE) {
+      lang = OVERRIDE_LANGUAGE;
+    }
+    else {
+      lang = this.getPrefferedLanguage();
+    }
+
+    if (!lang || IGNORED_TRANSLATIONS.includes(lang)) {
+      return;
+    }
+
+    const url = `${TRANSLATIONS_PATH}/${lang}/images.json`;
+    const resp = await fetch(url);
+    if (!resp.ok) {
+      return;
+    }
+
+    return await resp.json();
+  }
+
+  /**
+   * @param {Record<string, string>} translations
+   */
+  async translatePageImages(translations) {
+    for (const [cls, src] of Object.entries(translations)) {
+      [...this.doc.querySelectorAll(`.${cls}`)].forEach(
+        (value, index, array) => {
+          value.src = src;
         }
       );
     }
@@ -120,4 +202,4 @@ class JutSuper {
 }
 
 var logger = new Logger(LOGGING_ENABLED, console);
-new JutSuper(document, navigator);
+new JutSuper(document, navigator, location);
